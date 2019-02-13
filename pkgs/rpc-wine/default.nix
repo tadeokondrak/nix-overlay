4969c2a619c3aa17d873ea1fa52029d881593e0b
Tadeo Kondrak <me@tadeo.ca>
2019-02-12 22:37:09 -0700

    rpc-wine: fix build

--- pkgs/rpc-wine/default.nix
+++ pkgs/rpc-wine/default.nix
@@ -1,18 +1,33 @@
-{ stdenv, fetchurl, rapidjson, wineWowPackages }:
+{ stdenv, fetchFromGitHub, rapidjson, wine, wineWowPackages }:
 
 stdenv.mkDerivation rec {
   pname = "rpc-wine";
   version = "1.0.0";
-  src = fetchurl {
-    url = "https://github.com/Marc3842h/${name}/archive/${version}.tar.gz";
-    sha256 = "18jfg5ws2xykvvkblyq3frp9x5rjpvm7hfa6cb0mr7865b6fd350";
+  name = "${pname}-${version}";
+
+  src = fetchFromGitHub {
+    owner = "Marc3842h";
+    repo = pname;
+    rev = version;
+    sha256 = "05mchqfal2skb3bxm62a55rya8b5ql0zxiwjn41ijzcycfy1p825";
   };
-  nativeBuildInputs = [ wineWowPackages.stable ];
+
+  nativeBuildInputs = [ (if stdenv.targetPlatform.system == "i686-linux" then wine else wineWowPackages.stable) ];
   buildInputs = [ rapidjson ];
+
   buildPhase = ''
-    bash build.sh --skip-32-bit
+    bash build.sh --skip-${if stdenv.targetPlatform.system == "i686-linux" then "64" else "32"}-bit
   '';
+
   installPhase = ''
-    install bin64/discord-rpc.dll.so -D $out/lib/wine/discord-rpc.dll.so
+    install bin*/discord-rpc.dll.so -D $out/lib/wine/discord-rpc.dll.so
   '';
+
+  meta = with stdenv.lib; {
+    description = "discord-rpc.dll implementation for Wine allowing your Wine games to interact with your native Discord instance";
+    homepage = https://github.com/Marc3842h/rpc-wine;
+    license = licenses.mit;
+    maintainers = with maintainers; [ tadeokondrak ];
+    platforms = [ "i686-linux" "x86_64-linux" ];
+  };
 }
