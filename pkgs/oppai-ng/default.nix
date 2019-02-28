fb9265e42af298db9573092c9aaa69e242bd5ba8
Tadeo Kondrak <me@tadeo.ca>
2019-02-27 17:12:34 -0700

    oppai-ng: 2.1.0 -> 3.2.0

--- pkgs/oppai-ng/default.nix
+++ pkgs/oppai-ng/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "oppai-ng";
-  version = "2.1.0";
+  version = "3.2.0";
 
   src = fetchFromGitHub {
     owner = "Francesco149";
     repo = pname;
     rev = version;
-    sha256 = "0qxr7ybpm5g3la908vzk7f3gr384vprv8xq92kf7n1p1g85bbxrw";
+    sha256 = "1alsdn1ghcp8jma2w7waljlcxr445kj85az3q92klvkyayc19413";
   };
 
   buildPhase = ''
